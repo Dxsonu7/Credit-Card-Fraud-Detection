39c68a313dd9d6fe8a98544732fe56c5e8facc9d
Sonu Gupta <sonu72752@gmail.com>
2024-12-16 23:52:51 -0600

    load and check imbalance

--- model.R
+++ model.R
@@ -7,3 +7,15 @@ library(xgboost)         # XGBoost model
 library(keras)           # Artificial Neural Network
 library(yardstick)       # Model evaluation (AUC, confusion matrix)
 
+## Read data and define research
+
+data <- read_csv("creditcard.csv")
+
+# Basic structure and summary
+glimpse(data)
+summary(data)
+
+# Check for class imbalance
+data %>%
+  count(Class) %>%
+  mutate(percentage = n / sum(n) * 100)
