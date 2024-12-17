96654725f1a046b9e2088c859f9e481557c61411
Sonu Gupta <sonu72752@gmail.com>
2024-12-16 23:48:46 -0600

    libs

---
+++ model.R
@@ -0,0 +1,9 @@
+## Load Libaries
+
+library(tidyverse)       # Data manipulation and visualization
+library(caret)           # Train-test split, preprocessing
+library(recipes)         # Preprocessing steps
+library(xgboost)         # XGBoost model
+library(keras)           # Artificial Neural Network
+library(yardstick)       # Model evaluation (AUC, confusion matrix)
+
